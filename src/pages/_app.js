8c8d9623acae231287ae2de4b2762c781f579869
FadiliGCD <135276813+FadiliGCD@users.noreply.github.com>
2025-06-12 13:04:37 +0100

    trying to delete dark mode button to fix issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,17 +2,8 @@ import '@/styles/globals.css'
 import { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  const [dark, setDark] = useState(false)
-
-  useEffect(() => {
-    document.body.classList.toggle('dark', dark)
-  }, [dark])
-
-  return (
-    <>
-      <Component {...pageProps} toggleDark={() => setDark(!dark)} isDark={dark} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
+
 
 export default MyApp
